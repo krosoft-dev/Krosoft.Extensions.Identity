45b908da5385f806e3549feb4db8774f2b1486d3
Kévin ROBERT <kevin.robert.kr@gmail.com>
2025-08-23 01:30:08 +0200

    Rework IdentityService

--- tests/Krosoft.Extensions.Identity.Tests/Helpers/TokenHelperTests.cs
+++ tests/Krosoft.Extensions.Identity.Tests/Helpers/TokenHelperTests.cs
@@ -5,6 +5,8 @@ namespace Krosoft.Extensions.Identity.Tests.Helpers;
 [TestClass]
 public class TokenHelperTests
 {
+    private const string Secret = "abc";
+    private const string Salt = "cba";
     private readonly string _salt = "salt";
     private readonly string _secret = "secret";
 
@@ -41,5 +43,39 @@ public class TokenHelperTests
         var result2 = TokenHelper.CreateSignedToken(payloadJson, _secret, _salt);
 
         Check.That(result1).IsEqualTo(result2);
+    }
+
+    [TestMethod]
+    public void GetUnsignedPayload_Ok()
+    {
+        const string token = "eyJJZGVudGlmaWFudCI6IlRlc3QifQ==.d7oRy1R7F6nzCXT0vw75/d5afV+aAyVnEZg3/Oe8whk=";
+        var payload = "{\"Identifiant\":\"Test\"}";
+        var generateToken = TokenHelper.CreateSignedToken(payload, Secret, Salt);
+
+        Check.That(generateToken).IsEqualTo(token);
+
+        Check.That(TokenHelper.GetUnsignedPayload(token, Secret, Salt)).IsEqualTo(payload);
+    }
+
+    [TestMethod]
+    public void GetUnsignedPayload_Ko()
+    {
+        const string token = "eyJpZGVudGlmaWFudCI6Ik1vblRlbmFudElkIiwiZXhwIjoTY";
+
+        Check.That(TokenHelper.GetUnsignedPayload(token, Secret, Salt)).IsEqualTo(null);
+    }
+
+    [TestMethod]
+    public void GetUnsignedPayload_TokenNull()
+    {
+        Check.That(TokenHelper.GetUnsignedPayload(null, Secret, Salt)).IsEqualTo(null);
+    }
+
+    [TestMethod]
+    public void GetUnsignedPayload_TokenEmpty()
+    {
+        const string token = "";
+
+        Check.That(TokenHelper.GetUnsignedPayload(token, Secret, Salt)).IsEqualTo(null);
     }
 }
